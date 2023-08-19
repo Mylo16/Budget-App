8f1374a56a4af563775863db9dc4e185d01c39cb
eric <ericopokuantwi384@gmail.com>
2023-08-19 18:46:08 +0000

    fix deployment error

--- db/migrate/20230819101519_remove_column_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :confirmation_sent_at, :datetime
-    remove_column :users, :confirmed_at, :datetime
-  end
-end
